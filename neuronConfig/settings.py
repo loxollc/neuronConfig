7599408c82b8ed9e4eff90fd6bb6de60b095bd8d
Max Thompson <maxwellworks7@gmail.com>
2020-01-08 18:30:52 -0500

    prepping to deploy

--- neuronConfig/settings.py
+++ neuronConfig/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,6 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
   os.path.join(SITE_ROOT, 'static/'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
